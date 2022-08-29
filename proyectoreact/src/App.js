33c26ca17d8bddbba2f3786c00d9809f632c1da3
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-08-29 10:37:11 -0300

    api

--- proyectoreact/src/App.js
+++ proyectoreact/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Movies from './Component/Movies';
 
 function App() {
   return (
---
+++ proyectoreact/src/Component/Movies.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+//importar css
+
+class Movies extends Component {
+    constructor() {
+        super()
+        this.state = {
+            movies: [], //aparecer peliculas
+            nextUrl: ''
+        }
+    }
+
+    componentDidMount() {
+        //BUscamos datos
+        fetch('https://api.themoviedb.org/3/movie/550?api_key=ff0d15573865ddc49a8a0b0024148010')
+            .then(res => res.json())
+            .then(data => this.setState({
+                movies: data.results,
+                nextUrl: data.info.next
+            }))
+            .catch()
+    }
+
+    traerMas() {
+        //Traer la siguiente página de movies
+        fetch(this.state.nextUrl)
+            .then(res => res.json())
+            .then(data => this.setState({
+                movies: data.results.concat(this.state.movies),
+                nextUrl: data.info.next
+            }))
+            .catch()
+    }
+}
+
+export default Movies;
