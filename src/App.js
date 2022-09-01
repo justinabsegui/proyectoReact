a050032b5308f398a0e39518e3bd2a979a97ad96
Francomainardi <fmainardi@udesa.edu.ar>
2022-09-01 18:12:57 -0300

    series y seriesCard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Component/Navbar/Navbar";
 import Movies from './Component/Movies/Movies';
+import Serie from "./Component/Series/Series";
 import NotFound from "./Notfound/NotFound";
 import { Route, Switch } from 'react-router-dom'; //componente que me deja escribir rutas
 
