84ca62b12f39f4720d879071fb5710536bb0394b
Francomainardi <fmainardi@udesa.edu.ar>
2022-09-01 17:57:34 -0300

    CSS done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import Navbar from "./Component/Navbar/Navbar";
-import Movies from './Component/Movies';
+import Movies from './Component/Movies/Movies';
 import NotFound from "./Notfound/NotFound";
-import './App.css';
-
 import { Route, Switch } from 'react-router-dom'; //componente que me deja escribir rutas
 
 function App() {
   return (
     <React.Fragment>
+
       <Navbar />
       <h1>Movies</h1>
       <main>
         <Switch>
-        <Route path='/movies' exact={true} component={Movies} />
+          <Route path='/movies' exact={true} component={Movies} />
           <Route path='' component={NotFound} />
         </Switch>
 
       </main>
+
     </React.Fragment>
   );
 }
