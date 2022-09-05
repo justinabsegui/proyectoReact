b8653b2ad309ff1d55f5943d4cee6a0a35649ed5
Maria Justina Basombrío Seguí <jus@MJBSs-MacBook-Pro.local>
2022-09-05 10:47:30 -0300

    nada

---
+++ proyectoreact/src/Component/Buscador/Buscador.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+function Buscador() {
+
+    // render(){
+    //     return (
+    //         <React.Fragment>
+    //             <div>
+    //                 <form action="/results" method="GET" class="search-form">
+    //                     <input type="text" id="search" name="search" placeholder="Buscar películas y series" class="search-form_input"> </input>
+    //                     <button type="submit" class="search-form_button"></button>
+    //                 </form>
+    //             </div>
+
+    //         </React.Fragment>
+    //     )
+
+    // }
+}
+export default Buscador
+
+
--- proyectoreact/src/Component/Header/Header.js
+++ proyectoreact/src/Component/Header/Header.js
@@ -4,10 +4,7 @@ import Navbar from "../Navbar/Navbar";
 function Header() {
     return (
         <header>
-            <form action="/results" method="GET" class="search-form">
-                <input type="text" id="search" name="search" placeholder="Buscar películas y series" class="search-form_input"> </input>
-                    <button type="submit" class="search-form_button"></button>
-            </form>
+           
             <Navbar />
 
         </header>
