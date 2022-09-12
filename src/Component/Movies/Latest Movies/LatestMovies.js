584cd820c1dc8867f9e1e57d7ab3244da2654d0c
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-09-12 10:20:35 -0300

    loaders

--- src/Component/Movies/Latest Movies/LatestMovies.js
+++ src/Component/Movies/Latest Movies/LatestMovies.js
@@ -1,46 +1,55 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import MovieCard from "../MovieCard/MovieCard";
 
 
 class LatestMovies extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={
-            data:[]
+        this.state = {
+            data: []
         }
     }
 
-    componentDidMount( ){
+    componentDidMount() {
         fetch("https://api.themoviedb.org/3/movie/latest?api_key=7a176cc95147be6e695be2faf0e8ff9c&language=en-US")
-        .then(response =>response.json())
-        .then(data => this.setState(
-            {data: data.results}
-        ))
-        .catch(error => console.log('el error fue '+ error ))
+            .then(response => response.json())
+            .then(data => this.setState(
+                { data: data.results }
+            ))
+            .catch(error => console.log('el error fue ' + error))
     }
 
-    borrar(id){
+    borrar(id) {
         let peliculasFiltradas = this.state.peliculas.filter(unLatestMovies => unLatestMovies.id !== id);
         this.setState({
-         peliculas: peliculasFiltradas
+            peliculas: peliculasFiltradas
         })
-     }
- 
+    }
 
-    render(){
-        return(
-            <React.Fragment> 
-                 <button onClick={()=>this.traerMas()}> Traer más </button>
-                <div>
-                    <h2 className="TituloC">Latest Movies</h2>
-                </div>
-                <section className='card-container'>
-                    {this.state.peliculas.map((unLatestMovies, idx )=> <MovieCard key={unLatestMovies + idx} peliculas={unLatestMovies}  image={unLatestMovies.poster_path} title={unLatestMovies.title}/>)}
-                </section>
+
+    render() {
+        return (
+            <React.Fragment>
+                <button onClick={() => this.traerMas()}> Traer más </button>
+
+                {
+                    this.state.data.length === 0 ?
+                        <Loader />
+                        :
+                        <>
+                            <div>
+                                <h2 className="TituloC">Latest Movies</h2>
+                            </div>
+                            <section className='card-container'>
+                                {this.state.peliculas.map((unLatestMovies, idx) => <MovieCard key={unLatestMovies + idx} peliculas={unLatestMovies} image={unLatestMovies.poster_path} title={unLatestMovies.title} />)}
+                            </section>
+                        </>
+                }
+
             </React.Fragment>
         )
-            
-    }
-    }
 
- export default LatestMovies;
+    }
+}
+
+export default LatestMovies;
