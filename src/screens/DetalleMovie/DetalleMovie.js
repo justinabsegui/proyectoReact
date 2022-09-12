bc6a5aeb21ccab77cdd0b492bef11e138f287fbf
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-09-12 09:40:01 -0300

    screens

---
+++ src/screens/DetalleMovie/DetalleMovie.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import MovieCard from "../../Component/Movies/MovieCard/MovieCard";
+
+
+class DetalleMovies extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            data:[]
+        }
+    }
+
+    componentDidMount( ){
+        fetch("https://api.themoviedb.org/3/find/{external_id}?api_key=7a176cc95147be6e695be2faf0e8ff9c&language=en-US&external_source=imdb_id")
+        .then(response =>response.json())
+        .then(data => this.setState(
+            {data: data.results}
+        ))
+        .catch(error => console.log('el error fue '+ error ))
+    }
+
+    render(){
+        return(
+            <React.Fragment> 
+                <div>
+                    <h2 className="TituloC">Top Rated Movies</h2>
+                </div>
+                <section className='card-container'>
+                    {this.state.data.map((unMovies, idx )=> <MovieCard key={unMovies + idx} data={unMovies}  image={unMovies.poster_path} title={unMovies.title}/>)}
+                </section>
+            </React.Fragment>
+        )
+            
+    }
+    }
+
+ export default DetalleMovies;
--- src/screens/DetalleSeries/DetalleSeries.js
+++ src/screens/DetalleSeries/DetalleSeries.js
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
-import MovieCard from "../MovieCard/MovieCard";
+import DetalleSeries from '../DetalleSeries/DetalleSeries.js'
 
 
-class DetalleMovies extends Component {
+class DetalleSeries extends Component {
     constructor(props){
         super(props);
         this.state ={
@@ -12,6 +12,7 @@ class DetalleMovies extends Component {
 
     componentDidMount( ){
         fetch("https://api.themoviedb.org/3/find/{external_id}?api_key=7a176cc95147be6e695be2faf0e8ff9c&language=en-US&external_source=imdb_id")
+        // cambiar la api
         .then(response =>response.json())
         .then(data => this.setState(
             {data: data.results}
@@ -23,7 +24,7 @@ class DetalleMovies extends Component {
         return(
             <React.Fragment> 
                 <div>
-                    <h2 className="TituloC">Top Rated Movies</h2>
+                    <h2 className="TituloC"> Detalle series</h2>
                 </div>
                 <section className='card-container'>
                     {this.state.data.map((unMovies, idx )=> <MovieCard key={unMovies + idx} data={unMovies}  image={unMovies.poster_path} title={unMovies.title}/>)}
@@ -34,4 +35,4 @@ class DetalleMovies extends Component {
     }
     }
 
- export default DetalleMovies;
+ export default DetalleSeries;
