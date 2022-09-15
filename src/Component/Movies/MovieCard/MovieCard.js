ff8fde410886f5797cf0a137fb5e60edcd9e1b10
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-09-15 10:15:53 -0300

    ver mas ver menos

--- src/Component/Movies/MovieCard/MovieCard.js
+++ src/Component/Movies/MovieCard/MovieCard.js
@@ -8,7 +8,6 @@ class MovieCard extends Component {
             text: 'Ver más',
         }
     }
-    
     viewMore() {
         if (this.state.viewMore) {
             this.setState({
@@ -18,7 +17,20 @@ class MovieCard extends Component {
         } else {
             this.setState({
                 viewMore: true,
-                text: 'ver menos'
+                text: 'Ver menos'
+            })
+        }
+    }
+    favoritos() {
+        if (this.state.favoritos) {
+            this.setState({
+                favoritos: false,
+                text2: 'Agregar a favoritos'
+            })
+        } else {
+            this.setState({
+                favoritos: true,
+                text2: 'Quitar de favoritos'
             })
         }
     }
@@ -27,28 +39,26 @@ class MovieCard extends Component {
     //     //guardo ids en array dentro de localStorage
     //     console.log('Agregando y sacando favs')
 
+    render() {
+        // console.log(this.props);
+        return (
+            <article className='movie-card'>
 
+                <h2 className='extra'>{this.props.datosPersonaje.title}</h2>
+                <img src={`https://image.tmdb.org/t/p/w342/${this.props.image}`} alt="" />
+                <p className='extra'> Release date: {this.props.datosPersonaje.release_date}</p>
 
+                <p className='more' onClick={() => this.viewMore()}>{this.state.text}</p>
+                <p className='more' onClick={() => this.favoritos()}>{this.state.text2}</p>
 
-        render(){
-            // console.log(this.props);
-            return (
-                <article className='movie-card'>
-            
-                <h2 className= 'extra'>{this.props.datosPersonaje.title}</h2> 
-                  
-                    <img src={`https://image.tmdb.org/t/p/w342/${this.props.image}`} alt="" />
-                    <p className= 'extra'> Release date: {this.props.datosPersonaje.release_date}</p> 
-                    <p className='more'>Ver más</p>
-                    <section >
-                        <p className= 'extra'> {this.props.datosPersonaje.overview}</p> 
-                        <p className= 'extra'> Rating: {this.props.datosPersonaje.vote_average}</p> 
+                <section >
+                    <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}> {this.props.datosPersonaje.overview}</p>
+                    <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}> Rating: {this.props.datosPersonaje.vote_average}</p>
+                </section>
+            </article>
 
-                    </section>
-                </article>
-
-            )
-        }
-    } 
+        )
+    }
+}
 
 export default MovieCard;
