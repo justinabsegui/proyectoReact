42ae226bcceba4937133f73cc263e88b673b5c08
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-09-17 00:07:26 -0300

    favs

--- src/screens/Favoritos/Favoritos.js
+++ src/screens/Favoritos/Favoritos.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../../Component/Movies/MovieCard/MovieCard';
+import './favoritos.css';
 
 class Favoritos extends Component {
     constructor() {
@@ -11,48 +12,41 @@ class Favoritos extends Component {
     }
 
     componentDidMount() {
-
-        let recuperoStorage = localStorage.getItem('favoritos')
+        let favmov = []
+        let recuperoStorage = localStorage.getItem('favmov')
 
         if (recuperoStorage !== null) {
-            let favmov = JSON.parse(recuperoStorage) //es un array de ids
-            let movies = [];
+            favmov = JSON.parse(recuperoStorage) //es un array de ids
+            let favs = [];
 
             //recorrer el array y pedirla al endpoint por los datos de cada personaje.
-            favmov.forEach(unIdFavorito => {
+            favmov.forEach((id) => {
                 //pedir por cada id los datos del personaje
-                fetch(`https://api.themoviedb.org/3/search/movie/${unIdFavorito}api_key=7a176cc95147be6e695be2faf0e8ff9c&language=en-US&page=1&include_adult=true`)
+                fetch(`https://api.themoviedb.org/3/search/movie/${id}api_key=7a176cc95147be6e695be2faf0e8ff9c&language=en-US&page=1&include_adult=true`)
                     .then(res => res.json())
-                    .then(data => movies.push(data),
-                        this.setState(
+                    .then(unaMovie => favs.push(unaMovie))
+                    .then(() => this.setState(
                             {
-                                movies: movies,
+                                movies: favs,
                             }
-                        )
+                        ))
 
-                    )
-
-                    .catch(e => console.log(e))
-            })
-
-            console.log(movies);
+                        .catch(e => console.log('El error es' + e))
+                    })
+            }
+    }
+        render() {
+            return (
+                <React.Fragment>
+                    <h2>My favourites movies</h2>
+                    <section className='card-container'>
+                        {
+                            this.state.movies.map((unaMovie, idx) => <MovieCard key={unaMovie.title + idx} datosMovie={unaMovie} />)
+                        }
+                    </section>
+                </React.Fragment>
+            )
         }
 
-
     }
-
-    render() {
-        return (
-            <React.Fragment>
-                <h2>My favourites movies</h2>
-                <section className='card-container'>
-                    {
-                        this.state.movies.map((unaMovie, idx) => <MovieCard key={unaMovie.name + idx} datosMovie={unaMovie} />)
-                    }
-                </section>
-            </React.Fragment>
-        )
-    }
-
-}
 export default Favoritos;
