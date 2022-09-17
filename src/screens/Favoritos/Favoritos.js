d2bc7ee5494b12da24b18da304b7301ae787128b
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-09-17 18:42:57 -0300

    logica favs

--- src/screens/Favoritos/Favoritos.js
+++ src/screens/Favoritos/Favoritos.js
@@ -41,36 +41,7 @@ class Favoritos extends Component {
             console.log(pelis)
         }
     }
-    //version que tampoco funciona
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         movies: [], //Es array de objetos literales con cada movie
-    //         favoritos: true,
-    //     }
-    // }
-    // componentDidMount() {
-    //     let recuperoStorage = localStorage.getItem(‘favoritos’)
-    //     if (recuperoStorage !== null) {
-    //         let favmov = JSON.parse(recuperoStorage) //es un array de ids
-    //         let movies = [];
-    //         //recorrer el array y pedirla al endpoint por los datos de cada personaje.
-    //         favmov.forEach(unIdFavorito => {
-    //             //pedir por cada id los datos del personaje
-    //             fetch(`https://api.themoviedb.org/3/search/movie/${unIdFavorito}api_key=7a176cc95147be6e695be2faf0e8ff9c&language=en-US&page=1&include_adult=true`)
-    //                 .then(res => res.json())
-    //                 .then(data => movies.push(data),
-    //                     this.setState(
-    //                         {
-    //                             movies: movies,
-    //                         }
-    //                     )
-    //                 )
-    //                 .catch(e => console.log(e))
-    //         })
-    //         console.log(movies);
-    //     }
-    // }
+    
     render() {
         return (
             <React.Fragment>
@@ -78,7 +49,7 @@ class Favoritos extends Component {
                 <section className='card-container'>
                     {
                         this.state.movies.map((dataMovie, idx) => <MovieCard key={dataMovie + idx} overview={dataMovie.overview}  release_date={dataMovie.release_date} vote_average={dataMovie.vote_average}  image={dataMovie.poster_path} title={dataMovie.title} id={dataMovie.id} />)
-                        // this.state.movies.map((unaMovie, idx) => <MovieCard key={unaMovie.name + idx} datosMovie={unaMovie} />)
+                        
                     }
                 </section>
             </React.Fragment>
